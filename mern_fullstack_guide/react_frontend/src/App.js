d5b48840d4c26a12370fd12837f9098544baa986
Andrii Hlyvko <andriihlyvko@gmail.com>
2022-03-29 06:43:09 -0400

    added html+css course materials

--- mern_fullstack_guide/react_frontend/src/App.js
+++ mern_fullstack_guide/react_frontend/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+
+// react router imports
+import { Route, Switch, Redirect } from "react-router-dom";
+
+// our pages imports
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+import Layout from "./shared/components/layout/Layout";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/users" />
+        </Route>
+        <Route path="/users" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="*">
+          <h2>Page does not exist</h2>
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
